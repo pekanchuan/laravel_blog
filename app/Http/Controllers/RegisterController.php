2772923c99ba8830242a40ca179b8a21db0825fc
CHia <dozeuto@gmail.com>
2023-02-22 13:48:26 +0800

    automatic password hashing with mutators

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -19,12 +19,14 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        User::create($request->validate([
+        $attributes = $request->validate([
             'name' => 'required|max:255',
             'username' => 'required|max:255|min:3',
             'email' => 'required|email|max:255',
             'password' => 'required|min:7|max:255'
-        ]));
+        ]);
+
+        User::create($attributes);
 
         return redirect('/');
     }
