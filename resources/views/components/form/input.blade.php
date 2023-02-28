b5ec2cf3ff0691848b6c4ede310c667fc51053b8
CHia <dozeuto@gmail.com>
2023-02-28 14:47:10 +0800

    Extend the Admin Layout

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,14 +1,14 @@
-@props(['name', 'type' => 'text'])
+@props(['name'])
 
 <x-form.field>
     <x-form.label name="{{ $name }}"/>
 
-    <input type="{{ $type }}"
-           class="border border-gray-400 p-2 w-full"
+    <input class="border border-gray-200 p-2 w-full rounded"
            name="{{ $name }}"
            id="{{ $name }}"
            value="{{ old($name) }}"
            required
+        {{ $attributes }}
     >
 
     <x-form.error name="{{ $name }}"/>
