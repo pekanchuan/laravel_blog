947e22ec07d340f33fa90f81861175a3238f2567
CHia <dozeuto@gmail.com>
2023-02-28 10:37:32 +0800

    Limit Access to Only Admins

---
+++ app/Http/Middleware/MustBeAdministrator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MustBeAdministrator
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->user()?->username !== 'init') {
+            abort(Response::HTTP_FORBIDDEN);
+        }
+
+        return $next($request);
+    }
+}
